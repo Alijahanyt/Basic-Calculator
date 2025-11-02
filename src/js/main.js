243a2811b1ff30fa7b440ada0d8c70a17395e958
Ali jahan <ali.internet7@gmail.com>
2025-11-02 03:42:31 +0330

    linked js file and started logic

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const display = document.getElementById('screen');
+
+
+
+function appendToScreen(value) {
+    display.value += value;
+}
+
+function clearScreen() {
+    display.value = '';
+}
+
+function calculateResult() {
+
+
+}
