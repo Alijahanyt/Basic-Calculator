3520835f9c876f27186d0199c948c48b6802d281
Ali jahan <ali.internet7@gmail.com>
2025-11-05 01:13:15 +0330

    all the logic finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,26 @@
 const display = document.getElementById('screen');
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.number');
 
 
 function appendToScreen(value) {
-    display.value += value;
 
+    // gets the last input by the user
+    let lastUserInput = display.value.slice(-1);
+
+    // console.log(lastUserInput);
+
+    // prevent operators at the beginning
+    if ((value == "%" || value == "/" || value == "*" || value == "+" || value == "-" || value == ".") && display.value === "") return
+
+    // prevent multiple operators in a row
+    if ((lastUserInput == "%" || lastUserInput == "/" || lastUserInput == "*" || lastUserInput == "+" || lastUserInput == "-" || lastUserInput == ".") && display.value.includes(value) && (value == "%" || value == "/" || value == "*" || value == "+" || value == "-" || value == ".")) return
+
+    // replace the last operator with the new one if two operators are entered consecutively
+    if ((lastUserInput == "%" || lastUserInput == "/" || lastUserInput == "*" || lastUserInput == "+" || lastUserInput == "-" || lastUserInput == ".") && (value == "%" || value == "/" || value == "*" || value == "+" || value == "-" || value == ".")) {
+        display.value = display.value.slice(0, -1);
+    }
+
+    display.value += value;
 }
 
 function clearScreen() {
